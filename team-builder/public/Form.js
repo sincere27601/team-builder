7469fda7b747afc7908f0f5becc320b075d4680f
Mike Raysor <michaelraysor1@gmail.com>
2020-02-20 21:30:19 -0500

    Updating and redwriting code

---
+++ team-builder/public/Form.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+
+
+<form action="/action_page.php" id="form1">
+    <label for="fname">Jason:</label>
+    <input type="text" id="fname" name="Jason"><br><br>
+        <input type="submit" value="Submit">
+</form>
+
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname" form="form1">
+
--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+class App extends useState {
+    state = {
+        listitems: [
+            {
+                id: 0,
+                Name: "James",
+                Location: "Florida" 
+            },
+            {
+                id: 1,
+                Name: "Robert",
+                Location: "New York"
+            },
+            {
+                id: 2,
+                Name: "Edgar",
+                Location: "Chicago"
+            },
+            {
+                id: 3,
+                Name: "Susan",
+                Location: "Atlanta"
+            },
+            {
+                id: 4,
+                Name: "Mark",
+                Location: "Seattle"
+            }
+        ]
+    };
+
+    render() {
+        return (
+                <ul className="App">
+                    {this.state.listitems.map(listitem => (
+                        <li key={listitem.id} className={listitem.modifier}>
+                            {listitem.context}
+                        </li>
+                    ))}
+                </ul>
+            
+        );
+    }
+}
+
+export default Item;
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
